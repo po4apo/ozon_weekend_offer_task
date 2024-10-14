e4cf4e26419ba2b29b3b3431194f4010ed5983c8
po4apo <suhar20001@mail.ru>
2024-10-14 20:36:06 +0400

    Ошибки обнаруженные при статическом анализе кода
    	new file:   .gitignore
    	modified:   README.md
    	modified:   test_dogs.py
    	modified:   README.md

--- test_dogs.py
+++ test_dogs.py
@@ -8,16 +8,18 @@ class YaUploader:
     def __init__(self):
         pass
 
-    def create_folder(self, path, token):
-        url_create = 'https://cloud-api.yandex.net/v1/disk/resources'
+    def create_folder(self, path, token): # token лучше получать из окружения или вообще из 
+        # url и headers лучше вынести в конструктор
+        # ендпоинты лучше вынести в отдельный enum
+        url_create = 'https://cloud-api.yandex.net/v1/disk/resources' 
         headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token}'}
-        response = requests.put(f'{url_create}?path={path}', headers = headers)
+        response = requests.put(f'{url_create}?path={path}', headers = headers) # response лучше вернуть, либо вообще убрать
 
     def upload_photos_to_yd(self, token, path, url_file, name):
         url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token}'}
         params = {"path": f'/{path}/{name}', 'url': url_file, "overwrite": "true"}
-        resp = requests.post(url, headers=headers, params=params)
+        resp = requests.post(url, headers=headers, params=params) # response лучше вернуть, либо вообще убрать
 
 
 def get_sub_breeds(breed):
@@ -37,28 +39,34 @@ def get_urls(breed, sub_breeds):
     return url_images
 
 
-def u(breed):
+def u(breed): # кошмар, а не название. Название должно отражать суть того что делает функция, можно назвать upload_breed_images
     sub_breeds = get_sub_breeds(breed)
     urls = get_urls(breed, sub_breeds)
     yandex_client = YaUploader()
-    yandex_client.create_folder('test_folder', "AgAAAAAJtest_tokenxkUEdew")
+    # test_folder не стоит хардкодить в таком виде
+    # думаю лучше было бы вынести это в какую-то конфигурацию
+    yandex_client.create_folder('test_folder', "AgAAAAAJtest_tokenxkUEdew") 
     for url in urls:
-        part_name = url.split('/')
+        part_name = url.split('/') # чтобы такое не делать нужно использовать pathlib
         name = '_'.join([part_name[-2], part_name[-1]])
         yandex_client.upload_photos_to_yd("AgAAAAAJtest_tokenxkUEdew", "test_folder", url, name)
 
-
-@pytest.mark.parametrize('breed', ['doberman', random.choice(['bulldog', 'collie'])])
-def test_proverka_upload_dog(breed):
+# в данном случае случайный выбор породы не допустим, так как он влияет на результат теста
+# случайные данные не должные влиять на результат, иначе тест нельзя воспроизвести
+@pytest.mark.parametrize('breed', ['doberman', random.choice(['bulldog', 'collie'])]) 
+# proverka? названия нужно писать на английском
+def test_proverka_upload_dog(breed: str):
     u(breed)
     # проверка
     url_create = 'https://cloud-api.yandex.net/v1/disk/resources'
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth AgAAAAAJtest_tokenxkUEdew'}
     response = requests.get(f'{url_create}?path=/test_folder', headers=headers)
     assert response.json()['type'] == "dir"
-    assert response.json()['name'] == "test_folder"
-    assert True
-    if get_sub_breeds(breed) == []:
+    assert response.json()['name'] == "test_folder" # тут проверяется api yandex
+    assert True # лучшая проверка :), но так лучше не делать
+    # даже если тут нужен if, то только для assert len(response.json()['_embedded']['items'])
+    # цикл for без проблем можно вынести
+    if get_sub_breeds(breed) == []: 
         assert len(response.json()['_embedded']['items']) == 1
         for item in response.json()['_embedded']['items']:
             assert item['type'] == 'file'
@@ -71,3 +79,4 @@ def test_proverka_upload_dog(breed):
             assert item['name'].startswith(breed)
 
 
+# слишком много отступов в конце файла, достаточно одного
